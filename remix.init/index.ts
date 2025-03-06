2c19891062e4e75a56b772df24dabb031682f0d1
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2025-03-05 19:53:58 -0500

    try to update init code

--- remix.init/index.ts
+++ remix.init/index.ts
@@ -1,13 +1,29 @@
-const { execSync } = require("node:child_process");
-const crypto = require("node:crypto");
-const fs = require("node:fs/promises");
-const path = require("node:path");
+import { execSync } from "node:child_process";
+import * as crypto from "node:crypto";
+import { existsSync } from "node:fs";
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
 
-const toml = require("@iarna/toml");
-const PackageJson = require("@npmcli/package-json");
-const semver = require("semver");
+import * as toml from "@iarna/toml";
+import { PackageJson } from "@npmcli/package-json";
+import * as semver from "semver";
 
-const cleanupCypressFiles = ({ fileEntries, packageManager }) =>
+export type PackageManager = "yarn" | "pnpm" | "npm";
+
+export interface PackageManagerCommands {
+  exec: string;
+  lockfile: string;
+  name: string;
+  run: (script: string, args: string) => string;
+}
+
+const cleanupCypressFiles = ({
+  fileEntries,
+  packageManager,
+}: {
+  fileEntries: [string, string][];
+  packageManager: PackageManagerCommands;
+}) => 
   fileEntries.flatMap(([filePath, content]) => {
     const newContent = content.replace(
       new RegExp("npx tsx", "g"),
@@ -17,24 +33,38 @@ const cleanupCypressFiles = ({ fileEntries, packageManager }) =>
     return [fs.writeFile(filePath, newContent)];
   });
 
-const escapeRegExp = (string) =>
+const escapeRegExp = (item: string): string =>
   // $& means the whole matched string
-  string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  item.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
-const getPackageManagerCommand = (packageManager) =>
+/**
+ * Detects which package manager is used in the workspace based on the lock file.
+ */
+export function detectPackageManager(dir = ""): PackageManager {
+  return existsSync(path.join(dir, "yarn.lock"))
+    ? "yarn"
+    : existsSync(path.join(dir, "pnpm-lock.yaml"))
+      ? "pnpm"
+      : "npm";
+}
+
+const getPackageManagerCommand = (
+  packageManager: PackageManager,
+): PackageManagerCommands =>
   // Inspired by https://github.com/nrwl/nx/blob/bd9b33eaef0393d01f747ea9a2ac5d2ca1fb87c6/packages/nx/src/utils/package-manager.ts#L38-L103
   ({
     bun: () => ({
       exec: "bunx",
       lockfile: "bun.lockb",
       name: "bun",
-      run: (script, args) => `bun run ${script} ${args || ""}`,
+      run: (script: string, args: string) => `bun run ${script} ${args || ""}`,
     }),
     npm: () => ({
       exec: "npx",
       lockfile: "package-lock.json",
       name: "npm",
-      run: (script, args) => `npm run ${script} ${args ? `-- ${args}` : ""}`,
+      run: (script: string, args: string) =>
+        `npm run ${script} ${args ? `-- ${args}` : ""}`,
     }),
     pnpm: () => {
       const pnpmVersion = getPackageManagerVersion("pnpm");
@@ -45,7 +75,7 @@ const getPackageManagerCommand = (packageManager) =>
         exec: useExec ? "pnpm exec" : "pnpx",
         lockfile: "pnpm-lock.yaml",
         name: "pnpm",
-        run: (script, args) =>
+        run: (script: string, args: string) =>
           includeDoubleDashBeforeArgs
             ? `pnpm run ${script} ${args ? `-- ${args}` : ""}`
             : `pnpm run ${script} ${args || ""}`,
@@ -55,15 +85,16 @@ const getPackageManagerCommand = (packageManager) =>
       exec: "yarn",
       lockfile: "yarn.lock",
       name: "yarn",
-      run: (script, args) => `yarn ${script} ${args || ""}`,
+      run: (script: string, args: string) => `yarn ${script} ${args || ""}`,
     }),
   })[packageManager]();
 
-const getPackageManagerVersion = (packageManager) =>
+const getPackageManagerVersion = (packageManager = detectPackageManager()) =>
   // Copied over from https://github.com/nrwl/nx/blob/bd9b33eaef0393d01f747ea9a2ac5d2ca1fb87c6/packages/nx/src/utils/package-manager.ts#L105-L114
   execSync(`${packageManager} --version`).toString("utf-8").trim();
 
-const getRandomString = (length) => crypto.randomBytes(length).toString("hex");
+const getRandomString = (length: number) =>
+  crypto.randomBytes(length).toString("hex");
 
 const removeUnusedDependencies = (dependencies, unusedDependencies) =>
   Object.fromEntries(
@@ -72,15 +103,17 @@ const removeUnusedDependencies = (dependencies, unusedDependencies) =>
     ),
   );
 
-const updatePackageJson = ({ APP_NAME, packageJson, packageManager }) => {
+const updatePackageJson = ({
+  APP_NAME,
+  packageJson,
+  packageManager,
+}): void => {
   const {
     devDependencies,
     prisma: { seed: prismaSeed, ...prisma },
-    scripts: {
-      // eslint-disable-next-line no-unused-vars
-      "format:repo": _repoFormatScript,
-      ...scripts
-    },
+    scripts: { 
+      //"format:repo": _repoFormatScript, 
+      ...scripts },
   } = packageJson.content;
 
   packageJson.update({
@@ -100,14 +133,20 @@ const updatePackageJson = ({ APP_NAME, packageJson, packageManager }) => {
   });
 };
 
-const main = async ({ packageManager, rootDirectory }) => {
+const main = async () => {
+  const rootDirectory = ".";
+  const packageManager = detectPackageManager();
   const pm = getPackageManagerCommand(packageManager);
 
   const README_PATH = path.join(rootDirectory, "README.md");
   const FLY_TOML_PATH = path.join(rootDirectory, "fly.toml");
   const EXAMPLE_ENV_PATH = path.join(rootDirectory, ".env.example");
   const ENV_PATH = path.join(rootDirectory, ".env");
-  const DOCKERFILE_PATH = path.join(rootDirectory, "remix.init", "Dockerfile." + pm.packageManager );
+  const DOCKERFILE_PATH = path.join(
+    rootDirectory,
+    "remix.init",
+    "Dockerfile." + pm.name,
+  );
   const CYPRESS_SUPPORT_PATH = path.join(rootDirectory, "cypress", "support");
   const CYPRESS_COMMANDS_PATH = path.join(CYPRESS_SUPPORT_PATH, "commands.ts");
   const CREATE_USER_COMMAND_PATH = path.join(
@@ -124,7 +163,7 @@ const main = async ({ packageManager, rootDirectory }) => {
   const DIR_NAME = path.basename(rootDirectory);
   const SUFFIX = getRandomString(2);
 
-  const APP_NAME = (DIR_NAME + "-" + SUFFIX)
+  const APP_NAME: string = (DIR_NAME + "-" + SUFFIX)
     // get rid of anything that's not allowed in an app name
     .replace(/[^a-zA-Z0-9-_]/g, "-");
 
@@ -148,13 +187,13 @@ const main = async ({ packageManager, rootDirectory }) => {
     PackageJson.load(rootDirectory),
   ]);
 
-  const newEnv = env.replace(
+  const newEnv: string = env.replace(
     /^SESSION_SECRET=.*$/m,
     `SESSION_SECRET="${getRandomString(16)}"`,
   );
 
   const prodToml = toml.parse(prodContent);
-  prodToml.app = prodToml.app.replace(REPLACER, APP_NAME);
+  prodToml.app = prodToml.app.toString().replace(REPLACER, APP_NAME);
 
   const initInstructions = `
 - First run this stack's \`remix.init\` script and commit the changes it makes to your project.
@@ -219,18 +258,19 @@ const main = async ({ packageManager, rootDirectory }) => {
 Setup is almost complete. Follow these steps to finish initialization:
 
 - Start the database:
-  ${pm.run("docker")}
+  ${pm.run("docker", "")}
 
 - Run setup (this updates the database):
-  ${pm.run("setup")}
+  ${pm.run("setup", "")}
 
 - Run the first build (this generates the server you will run):
-  ${pm.run("build")}
+  ${pm.run("build", "")}
 
 - You're now ready to rock and roll 🤘
-  ${pm.run("dev")}
+  ${pm.run("dev", "")}
     `.trim(),
   );
 };
 
-module.exports = main;
+// module.exports = main;
+main().catch(console.error);
